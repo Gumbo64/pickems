1d7fa1c6e2d7caa5f1814525b1628ec9a9a3b8f5
Rory Coughlan <35547211+Gumbo64@users.noreply.github.com>
2025-01-19 01:12:18 +1100

    Added google logins

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
-
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 const auth = getAuth();
 
 const Login = () => {
@@ -22,6 +22,35 @@ const Login = () => {
       });
   };
 
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+  
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential === null) {
+          throw new Error('Google Auth Error');
+        }
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log([token, user]);
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log([errorCode, errorMessage, email, credential]);
+        // ...
+      });
+    };
+
   return (
     <div>
       <form onSubmit={handleLogin}>
@@ -40,6 +69,7 @@ const Login = () => {
         <button type="submit">Login</button>
       </form>
       {error && <p>{error}</p>}
+      <button onClick={handleGoogleLogin}>Sign in with Google</button>
 
       <div className="password-forgot">
         <Link to="/password-forgot">Forgot your Password?</Link>
